871f9fba60a9cb63e84dc2c8448de9fd02a93801
Diyar Kudrat <dkudrat@gmail.com>
2020-10-06 20:47:55 -0700

    able to add candidate

--- contracts/Election.sol
+++ contracts/Election.sol
@@ -1,9 +1,26 @@
 pragma solidity ^0.5.16;
 
 contract Election {
-    string public candidate;
-    // constructor
+    // Candidate Model
+    struct Candidate {
+        uint id;
+        string name;
+        uint voteCount;
+    }
+
+    // Fetch candidates
+    mapping(uint => Candidate) public candidates;
+
+    // Store count of how many candidates there are
+    uint public candidatesCount;
+
     constructor () public {
-        candidate = "Candidate 1";
+        addCandidate("Candidate 1");
+        addCandidate("Candidate 2");
+    }
+
+    function addCandidate (string memory _name) private {
+        candidatesCount ++;
+        candidates[candidatesCount] = Candidate(candidatesCount, _name, 0);
     }
 }
